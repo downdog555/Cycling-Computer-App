053e29b4c69e9c2806f0460af46803c0f8fad3fb
Reece Smith <downdog555@googlemail.com>
2018-02-04 16:09:15 +0000

    Extended HRData processing complete as well as conversion, we create two different lists for the euro and us vaues. TODO process cycling paramters section to get summary data, as well as the header records

---
+++ CyclingApp/CyclingApp/HrData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclingApp
+{
+    /// <summary>
+    /// Class to represent and process the hr data section
+    /// </summary>
+    class HrData
+    {
+        private List<HrDataSingle> DataEuro;
+        private List<HrDataSingle> DataUS;
+
+        public HrData(Boolean dataType, int version, List<string> rawData, int cadAlt = 46)
+        {
+            //means euro data type
+            if (!dataType)
+            {
+                DataEuro = new List<HrDataSingle>();
+                DataUS = new List<HrDataSingle>();
+                foreach (string line in rawData)
+                {
+                    HrDataSingle tempEuro = new HrDataSingle(line.Split('\t').ToList<string>(), version, cadAlt);
+
+                    //we need to convert form euro to us 
+                    List<string> lineConverted = new List<string>();
+                    lineConverted.Add("" + tempEuro.HeartRate);
+                    lineConverted.Add("" + (tempEuro.Speed * 0.6213711922));
+                    if (cadAlt == 0)
+                    {
+                        lineConverted.Add("" + tempEuro.Cadence);
+                    }
+                    else if (cadAlt == 1)
+                    {
+                        lineConverted.Add("" + (tempEuro.Altitude * 3.280839895));
+                    }
+                    else
+                    {
+                        lineConverted.Add("" + tempEuro.Cadence);
+                        lineConverted.Add("" + (tempEuro.Altitude * 3.280839895));
+                    }
+                    lineConverted.Add("" + tempEuro.Power);
+                    lineConverted.Add("" + tempEuro.PbPedInd);
+                    lineConverted.Add("" + tempEuro.AirPressure);
+                    DataUS.Add(new HrDataSingle(lineConverted, version, cadAlt));
+                    DataEuro.Add(tempEuro);
+                }
+            }
+            else
+            {
+                foreach (string line in rawData)
+                {
+                    DataUS = new List<HrDataSingle>();
+                    DataEuro = new List<HrDataSingle>();
+
+                    HrDataSingle tempUS = new HrDataSingle(line.Split('\t').ToList<string>(), version, cadAlt);
+                    //we need to convert form us to 
+                    List<string> lineConverted = new List<string>();
+                    lineConverted.Add(""+tempUS.HeartRate);
+                    lineConverted.Add(""+(tempUS.Speed* 1.609344));
+                    if (cadAlt == 0)
+                    {
+                        lineConverted.Add("" + tempUS.Cadence);
+                    }
+                    else if (cadAlt == 1)
+                    {
+                        lineConverted.Add("" + (tempUS.Altitude * 0.3048));
+                    }
+                    else
+                    {
+                        lineConverted.Add("" + tempUS.Cadence);
+                        lineConverted.Add("" + (tempUS.Altitude * 0.3048));
+                    }
+                    lineConverted.Add(""+tempUS.Power);
+                    lineConverted.Add(""+tempUS.PbPedInd);
+                    lineConverted.Add(""+tempUS.AirPressure);
+                    DataEuro.Add(new HrDataSingle(lineConverted, version, cadAlt));
+                    DataUS.Add(tempUS);
+                }
+            }
+
+        }
+    }
+}
---
+++ CyclingApp/CyclingApp/HrDataSingle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclingApp
+{
+    /// <summary>
+    /// Struct to represent a single line in the hr data
+    /// </summary>
+    struct HrDataSingle
+    {
+        private int heartRate, cadence, altitude, power, airPressure;
+        /// <summary>
+        /// Power balance and pedaling index
+        /// </summary>
+        private int pbPedInd;
+        private double speed;
+        
+
+        public HrDataSingle(List<string> data, int version, int cadAlt = 46)
+        {
+            heartRate = cadence = altitude = power = airPressure = pbPedInd = 0;
+            speed = 0.0;
+            if (data.Count == 3)
+            {
+                heartRate = Convert.ToInt32(data.ElementAt(0));
+                speed = Convert.ToDouble(data.ElementAt(1))/10;
+                if (cadAlt == 0)
+                {
+                    cadence = Convert.ToInt32(data.ElementAt(2));
+                }
+                else if(cadAlt == 1)
+                {
+                    altitude = Convert.ToInt32(data.ElementAt(2)); ;
+                }
+            }
+            else if (data.Count == 6)
+            {
+                heartRate = Convert.ToInt32(data.ElementAt(0));
+                speed = Convert.ToDouble(data.ElementAt(1)) / 10;
+                cadence = Convert.ToInt32(data.ElementAt(2));
+                altitude = Convert.ToInt32(data.ElementAt(3));
+                power = Convert.ToInt32(data.ElementAt(4));
+                pbPedInd = Convert.ToInt32(data.ElementAt(5));
+
+
+            }
+            else if (data.Count == 7)
+            {
+                heartRate = Convert.ToInt32(data.ElementAt(0));
+                speed = Convert.ToDouble(data.ElementAt(1)) / 10;
+                cadence = Convert.ToInt32(data.ElementAt(2));
+                altitude = Convert.ToInt32(data.ElementAt(3));
+                power = Convert.ToInt32(data.ElementAt(4));
+                pbPedInd = Convert.ToInt32(data.ElementAt(5));
+                airPressure = Convert.ToInt32(data.ElementAt(6));
+            }
+        }
+
+        public int HeartRate { get => heartRate; }
+        public int Cadence { get => cadence;  }
+        public int Altitude { get => altitude;  }
+        public int Power { get => power;  }
+        public int AirPressure { get => airPressure;  }
+        public int PbPedInd { get => pbPedInd; }
+        public double Speed { get => speed; }
+    }
+}
